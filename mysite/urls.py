775d13e1c6ae2956c3a7eb630228b8214e0a68e1
Vinicius Rezende <vgrezende@gmail.com>
2018-10-10 18:11:25 -0300

    7/14

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,8 @@
+from . import views
 from django.urls import path
-from .views import index
 
 urlpatterns = [
-    path('', index),
+    path('', views.index, name='home'),
+    path('contact', views.contact, name='contact'),
+    path('portfolio', views.portfolio, name='portfolio'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'mysite/index.html')
+
+def contact(request):
+    return render(request, 'mysite/contact.html')
+
+def portfolio(request):
+    return render(request, 'mysite/portfolio.html')
