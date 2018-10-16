80d0c3fe62b62cd60b4c6ea62f9a6784a5fb813e
Vinicius Rezende <vgrezende@gmail.com>
2018-10-16 17:24:50 -0300

    Create contact form

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Contact(models.Model):
+    email = models.EmailField()
+    subject = models.CharField(max_length=196)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.email
--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
 
@@ -9,4 +10,9 @@ def contact(request):
     return render(request, 'mysite/contact.html')
 
 def portfolio(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        Contact.objects.save(email=email,subject=subject,message=message)
     return render(request, 'mysite/portfolio.html')
